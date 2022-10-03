3ad5752a7bf370525f6803efc6411f9fa1628687
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-10-03 10:40:15 +0530

    commit

---
+++ 0.0.1-junit1/src/main/java/com/kpi1/_junit1/App.java
@@ -0,0 +1,13 @@
+package com.kpi1._junit1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
